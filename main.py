0b14e142e26b7fb5e920c474d7afabd93a4d5d4b
anjilinux <anjireddy2024@gmail.com>
2026-02-01 20:48:37 +0530

    loan-eb

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from schema import LoanInput
 
 app = FastAPI(title="Loan Eligibility Prediction API")
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 model = joblib.load("artifacts/model.pkl")
 
 @app.post("/predict")
 def predict_loan(data: LoanInput):
-    df = pd.DataFrame([data.dict()])
-    pred = model.predict(df)[0]
-    return {"loan_status": "Approved" if pred == 1 else "Rejected"}
+    try:
+        df = pd.DataFrame([data.dict()])
+        pred = model.predict(df)[0]
+        return {"loan_status": "Approved" if pred == 1 else "Rejected"}
+    except Exception as e:
+        return {"error": str(e)}
